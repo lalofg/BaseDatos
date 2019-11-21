421595fb6da90cdba782153828db96d90b988154
jorbarr <jorge.barrio@alumnos.uva.es>
2019-11-22 00:54:15 +0100

    Casi todo depurado

--- parking.sql
+++ parking.sql
@@ -3,12 +3,22 @@ CREATE TYPE tipoPlaza AS ENUM ('rotacional', 'residencial');
 CREATE TYPE tipoVehiculo AS ENUM ('motocicleta', 'turismo', 'autocaravana');
 CREATE TYPE tipoServicioComplementario AS ENUM ('lavado', 'reparacion', 'sustitucionLunas', 'micrologistica', 'alquilerBicicletas');
 CREATE TYPE tipoEmisiones AS ENUM ('ECO', 'CERO', 'B', 'C');
-CREATE TYPE estadoSolicitud AS ENUM ('aprobado', 'pendiente', 'rechazado')
-CREATE TYPE tipoSolicitud AS ENUM ('conReserva24', 'conReservaCesionDeUso', 'sinReservaDiurno', 'sinReservaNocturno')
+CREATE TYPE estadoSolicitud AS ENUM ('aprobado', 'pendiente', 'rechazado');
+CREATE TYPE tipoSolicitud AS ENUM ('conReserva24', 'conReservaCesionDeUso', 'sinReservaDiurno', 'sinReservaNocturno');
 
 
 DROP TABLE Aparcamiento;
-DROP TABLE 
+DROP TABLE Plaza;
+DROP TABLE ServicioComplementario;
+DROP TABLE ReclamacionesYSugerencias;
+DROP TABLE Ingresos;
+DROP TABLE Vehiculos;
+DROP TABLE RegistroEntradaSalida;
+DROP TABLE Empleado;
+DROP TABLE Usuario;
+DROP TABLE Solicitud;
+DROP TABLE Abono;
+
 CREATE TABLE Aparcamiento (                                                     
     nombre char(20),
     direccion char(20) NOT NULL UNIQUE,
@@ -24,6 +34,7 @@ CREATE TABLE Aparcamiento (
 );
 
 CREATE TABLE Plaza (
+    nombreAparcamiento char(20),
     numPlaza INTEGER,
     tipo tipoPlaza NOT NULL,
     accesible bit NOT NULL,
@@ -36,14 +47,17 @@ CREATE TABLE Plaza (
 );
 
 CREATE TABLE ServicioComplementario (
+    nombreAparcamiento char(20),
+    numPlaza INTEGER,
     tipo tipoServicioComplementario,
     precio FLOAT,
     PRIMARY KEY (tipo),
-    FOREIGN KEY (numPlaza) REFERENCES Plaza (numPlaza),
-    FOREIGN KEY (nombreAparcamiento) REFERENCES Aparcamiento (nombre)
+    FOREIGN KEY (nombreAparcamiento) REFERENCES Aparcamiento (nombre),
+    FOREIGN KEY (numPlaza) REFERENCES Plaza (numPlaza)
 );
 
 CREATE TABLE ReclamacionesYSugerencias (
+    nombreAparcamiento char(20),
     cod INTEGER,
     texto text NOT NULL,
     primary key (cod),
@@ -51,20 +65,11 @@ CREATE TABLE ReclamacionesYSugerencias (
 );
 
 CREATE TABLE Ingresos (
+    nombreAparcamiento char(20),
     fecha date,
     dinero  FLOAT NOT NULL,
     PRIMARY KEY (fecha),
     FOREIGN KEY (nombreAparcamiento) REFERENCES Aparcamiento (nombre)
-);
-
-CREATE TABLE RegistroEntradaSalida (
-    /*idReg INTEGER,*/
-    entrada datetime NOT NULL,
-    salida datetime,
-    importe FLOAT,                  /*CHECK si el vehiculo tiene un abolo debe ser 0*/ 
-    PRIMARY KEY (entredada),
-    FOREIGN KEY (nombreAparcamiento) REFERENCES Aparcamiento (nombre),
-    FOREIGN KEY (matrVehiculo) REFERENCES Vehiculo (matricula)
 );
 
 CREATE TABLE Vehiculo (
@@ -73,10 +78,41 @@ CREATE TABLE Vehiculo (
     tipo tipoVehiculo NOT NULL,
     emisiones tipoEmisiones NOT NULL,
     PRIMARY KEY (matricula),
-    FOREIGN KEY (abono) REFERENCES Abono (idAbono)  /*Asi o que el abono guarde vehiculos 1..5*/
+);
+
+CREATE TABLE RegistroEntradaSalida (
+    nombreAparcamiento char(20),
+    matrVehiculo char(20),
+    entrada datetime NOT NULL,
+    salida datetime,
+    importe FLOAT,                  /*CHECK si el vehiculo tiene un abolo debe ser 0*/ 
+    PRIMARY KEY (entredada),
+    FOREIGN KEY (nombreAparcamiento) REFERENCES Aparcamiento (nombre),
+    FOREIGN KEY (matrVehiculo) REFERENCES Vehiculo (matricula)
+);
+
+CREATE TABLE Empleado(
+    nombreAparcamiento char(20),
+    numIdentificacion char(20),
+    apellidos char[40] NOT NULL,
+    direccion char[30] NOT NULL,
+    esGestor bit,
+    PRIMARY KEY(numIdentificacion),
+    FOREIGN KEY(nombreAparcamiento) REFERENCES Aparcamiento(nombre)
+);
+
+CREATE TABLE Usuario(
+    numIdentificacion char(20),
+    nombre char(20) NOT NULL,
+    apellidos char(40) NOT NULL,
+    direccion char(30) NOT NULL,
+    pmr bit NOT NULL,
+    PRIMARY KEY(numIdentificador)
 );
 
 CREATE TABLE Solicitud(
+    nombreAparcamiento char(20),
+    usuario char(20),
     fecha datetime,
     estado estadoSolicitud NOT NULL,
     acreditacion char(10000) NOT NULL,
@@ -87,25 +123,10 @@ CREATE TABLE Solicitud(
     FOREIGN KEY(nombreAparcamiento) REFERENCES Aparcamiento(nombre)
 );
 
-CREATE TABLE Usuario(
-    numIdentificacion char[20],
-    nombre cahr[20] NOT NULL,
-    apellidos char[40] NOT NULL,
-    direccion char[30] NOT NULL,
-    pmr bit NOT NULL,
-    PRIMARY KEY(numIdentificador)
-);
-
-CREATE TABLE Empleado(
-    numIdentificacion char[20]
-    apellidos char[40] NOT NULL,
-    direccion char[30] NOT NULL,
-    esGestor bit,
-    PRIMARY KEY(numIdentificacion),
-    FOREIGN KEY(nombreAparcamiento) REFERENCES Aparcamiento(nombre)
-);
-
 CREATE TABLE Abono(
+    nombreAparcamiento char(20),
+    vehiculos char(20),
+    plaza INTEGER,
     idAbono char[10],
     precio FLOAT,
     gastosRepercutibles FLOAT,
@@ -115,12 +136,15 @@ CREATE TABLE Abono(
     mesesEnMorosidad INTEGER,
     horaEntrada time,
     horaSalida time,
-    
     PRIMARY KEY(idAbono),
-    FOREIGN KEY(Vehiculo) REFERENCES Aparcamiento(matriculaVehiculo),
-    FOREIGN KEY(Plaza) REFERENCES Aparcamiento(numPlaza)
+    FOREIGN KEY(vehiculo) REFERENCES Aparcamiento(matriculaVehiculo),
+    FOREIGN KEY(plaza) REFERENCES Aparcamiento(numPlaza)
 );
 
-
-
-
+/************************************** Aparcamiento **************************************/
+/*
+INSERT INTO Aparcamiento VALUES ('UVa', 'C/ Uno', '08:00:00', '20:00:00', 'disponible', 1, 0, 0.1, 0.1, 0.2);
+INSERT INTO Plaza VALUES ('UVa', 1, 1, 1, 0, 0, 'turismo');
+INSERT INTO ServicioComplementario VALUE ('UVa', 'lavado', 15.3, 'UVa', 1);
+*/
+/******************************************************************************************/
